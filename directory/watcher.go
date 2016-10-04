44f9a6c109260e95935005fe38b101ada3ab0853
Stefan Smarzly <smarzlys@gmail.com>
2016-10-04 17:16:00 +0200

    watcher: support for parallel connections from a peer (based on a counter)

--- directory/watcher.go
+++ directory/watcher.go
@@ -7,8 +7,13 @@ import (
 	auth "github.com/grandcat/srpc/authentication"
 )
 
+type peerStatus struct {
+	peer       *PeerInfo
+	aliveConns uint
+}
+
 type peerWatcher struct {
-	peersOn map[auth.PeerID]*PeerInfo
+	peersOn map[auth.PeerID]peerStatus
 	mu      sync.RWMutex // Needed if GW wants a list of available peers
 
 	notifies chan stateChange
@@ -21,7 +26,7 @@ type stateChange struct {
 
 func newPeerWatcher() *peerWatcher {
 	w := &peerWatcher{
-		peersOn:  make(map[auth.PeerID]*PeerInfo),
+		peersOn:  make(map[auth.PeerID]peerStatus),
 		notifies: make(chan stateChange), // cap = 0, blocking
 	}
 
@@ -37,21 +42,46 @@ func (w *peerWatcher) watch() {
 		// Update our view on available nodes
 		if n.avail == 0 {
 			// Peer went offline (temporary or forever)
-			w.mu.Lock()
-			delete(w.peersOn, n.p.ID)
-			w.mu.Unlock()
+			w.delOrDec(n.p)
 
-			log.Printf("[%s] Offline or unreachable", n.p.ID)
 		} else {
-			// Peer went online
-			w.mu.Lock()
-			w.peersOn[n.p.ID] = n.p
-			w.mu.Unlock()
-
-			log.Printf("[%s] Online", n.p.ID)
+			// Peer went online or added another simultaneous connection.
+			w.addOrInc(n.p)
 		}
 		// List peers currently available
 		log.Println("Online peers:", w.peersOn)
+	}
+}
+
+func (w *peerWatcher) addOrInc(peer *PeerInfo) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+
+	if s, ok := w.peersOn[peer.ID]; ok {
+		s.aliveConns++
+		w.peersOn[peer.ID] = s
+		log.Printf("[%s] %d active conns", peer.ID, s.aliveConns)
+
+	} else {
+		w.peersOn[peer.ID] = peerStatus{peer: peer, aliveConns: 1}
+		log.Printf("[%s] Online", peer.ID)
+	}
+}
+
+func (w *peerWatcher) delOrDec(peer *PeerInfo) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+
+	if s, ok := w.peersOn[peer.ID]; ok {
+		if s.aliveConns == 1 {
+			delete(w.peersOn, peer.ID)
+			log.Printf("[%s] Offline or unreachable", peer.ID)
+
+		} else {
+			s.aliveConns--
+			w.peersOn[peer.ID] = s
+			log.Printf("[%s] %d active conns", peer.ID, s.aliveConns)
+		}
 	}
 }
 
@@ -64,8 +94,8 @@ func (w *peerWatcher) AvailablePeers() []*PeerInfo {
 	defer w.mu.RUnlock()
 
 	nodes := make([]*PeerInfo, 0, len(w.peersOn))
-	for _, p := range w.peersOn {
-		nodes = append(nodes, p)
+	for _, ps := range w.peersOn {
+		nodes = append(nodes, ps.peer)
 	}
 
 	return nodes
