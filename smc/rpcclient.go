c7bc68dbc308301eaa39315c0e51959618ce21e1
Stefan Smarzly <smarzlys@gmail.com>
2016-10-23 17:08:39 +0200

    smc connector: use unix socket for local communication with Fresco instance

--- smc/rpcclient.go
+++ smc/rpcclient.go
@@ -2,20 +2,35 @@ package smc
 
 import (
 	"context"
+	"errors"
 	"log"
+	"net"
+	"strings"
+	"time"
 
 	pbJob "github.com/grandcat/flexsmc/proto/job"
 	proto "github.com/grandcat/flexsmc/proto/smc"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 )
 
 const (
-	serverAddr  = "localhost:50052"
-	defaultName = "world"
+	serverAddr     = "localhost:50052"
+	serverAddrSock = "unix:/tmp/grpc.sock"
+	defaultName    = "world"
 )
 
 func connect() {
-	conn, err := grpc.Dial(serverAddr, grpc.WithInsecure())
+	dialSocket := func(addr string, timeout time.Duration) (net.Conn, error) {
+		const netSep = "unix:"
+		isUnix := strings.HasPrefix(addr, netSep)
+		if !isUnix {
+			return nil, errors.New("unknown network type")
+		}
+		return net.DialTimeout(addr[:len(netSep)-1], addr[len(netSep):], timeout)
+	}
+
+	conn, err := grpc.Dial(serverAddrSock, grpc.WithDialer(dialSocket), grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
@@ -23,13 +38,15 @@ func connect() {
 
 	c := proto.NewSMCClient(conn)
 
+	// Session metadata as header
+	md := metadata.Pairs("session-id", "123456789")
+	ctx := metadata.NewContext(context.Background(), md)
 	// 1. Init
-	r, err := c.Init(context.Background(), &proto.SessionCtx{SessionID: 1234})
+	r, err := c.Init(ctx, &proto.SessionCtx{SessionID: 12345})
 	if err != nil {
 		log.Fatalf("could not SMC Init: %v", err)
 	}
 	log.Printf("Init: %s", r)
-
 	// 2. Prepare
 	m := &pbJob.PreparePhase{
 		Participants: []*pbJob.PreparePhase_Participant{
@@ -41,7 +58,7 @@ func connect() {
 			},
 		},
 	}
-	r, err = c.DoPrepare(context.Background(), m)
+	r, err = c.DoPrepare(ctx, m)
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
