0e675ca704642b95ec5a2aedcb1fc7de4c47dc36
Stefan Smarzly <smarzlys@gmail.com>
2016-11-02 14:48:32 +0100

    smc connector: minimal socket connector for gRPC server + moved full SMC test to _test.go file

--- smc/rpcclient.go
+++ smc/rpcclient.go
@@ -1,122 +1,26 @@
 package smc
 
 import (
-	"context"
 	"errors"
-	"log"
 	"net"
 	"strings"
 	"time"
 
-	pbJob "github.com/grandcat/flexsmc/proto/job"
-	proto "github.com/grandcat/flexsmc/proto/smc"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/metadata"
 )
 
 const (
-	defServerSocket = "unix:/tmp/grpc.sock"
+	defaultSocket = "unix:/tmp/grpc.sock"
 )
 
-func connect(socket string, id int32) {
-	dialSocket := func(addr string, timeout time.Duration) (net.Conn, error) {
-		const netSep = "unix"
-		isUnixSock := strings.HasPrefix(addr, netSep)
-		s := strings.Index(addr, ":")
-		if !isUnixSock || s < 0 {
-			return nil, errors.New("unknown network type")
-		}
-		return net.DialTimeout(addr[:s], addr[s+1:], timeout)
-	}
-
+// DialSocket connects to a gRPC server via a local socket.
+func DialSocket(socket string) (*grpc.ClientConn, error) {
 	if socket == "" {
-		socket = defServerSocket
-	}
-
-	conn, err := grpc.Dial(socket, grpc.WithDialer(dialSocket), grpc.WithInsecure())
-	if err != nil {
-		log.Printf("did not connect: %v", err)
-		return
-	}
-	defer conn.Close()
-
-	c := proto.NewSMCClient(conn)
-
-	ctx, _ := context.WithTimeout(context.Background(), time.Millisecond*500)
-	// 1. Init
-	r, err := c.Init(ctx, &proto.SessionCtx{SessionID: "123456789"})
-	if err != nil {
-		log.Printf("could not SMC Init: %v", err)
-		return
-	}
-	log.Printf("Init: %s", r)
-	// 2. Prepare (and second prepare with one missing participant)
-	md := metadata.Pairs("session-id", "123456789")
-	ctx = metadata.NewContext(ctx, md)
-
-	for i := 0; i < 2; i++ {
-		m := &pbJob.SMCCmd{
-			SmcPeerID: id,
-			Payload: &pbJob.SMCCmd_Prepare{Prepare: &pbJob.PreparePhase{
-				Participants: []*pbJob.PreparePhase_Participant{
-					&pbJob.PreparePhase_Participant{
-						SmcPeerID: 1,
-						Endpoint:  "[::1]:10001",
-					},
-					&pbJob.PreparePhase_Participant{
-						SmcPeerID: 2,
-						Endpoint:  "[::1]:10002",
-					},
-					&pbJob.PreparePhase_Participant{
-						SmcPeerID: 3,
-						Endpoint:  "[::1]:10003",
-					},
-					&pbJob.PreparePhase_Participant{
-						SmcPeerID: 4,
-						Endpoint:  "[::1]:10004",
-					},
-				},
-			}},
-		}
-		// Simulate a situation removing one node previously available
-		if i == 1 {
-			pl := m.Payload.(*pbJob.SMCCmd_Prepare).Prepare.Participants
-			pl = pl[:len(pl)-1]
-			m.Payload.(*pbJob.SMCCmd_Prepare).Prepare.Participants = pl
-		}
-
-		r, err = c.NextCmd(ctx, m)
-		if err != nil {
-			log.Fatalf("cmd for next phase failed: %v", err)
-		}
-		log.Printf("NextCmd: %s", r)
-	}
-
-	// 3. Start session
-	m := &pbJob.SMCCmd{
-		SmcPeerID: id,
-		Payload:   &pbJob.SMCCmd_Session{Session: &pbJob.SessionPhase{}},
-	}
-	r, err = c.NextCmd(ctx, m)
-	if err != nil {
-		log.Fatalf("cmd for next phase failed: %v", err)
-	}
-	log.Printf("NextCmd Session: %s", r)
-}
-
-func DialSMCClient(socket string) (proto.SMCClient, error) {
-	if socket == "" {
-		socket = defServerSocket
+		socket = defaultSocket
 	}
 
 	conn, err := grpc.Dial(socket, grpc.WithDialer(socketDialer), grpc.WithInsecure())
-	if err != nil {
-		log.Printf("could not connect to socket: %v", err)
-		return nil, err
-	}
-
-	cl := proto.NewSMCClient(conn)
-	return cl, nil
+	return conn, err
 }
 
 func socketDialer(addr string, timeout time.Duration) (net.Conn, error) {
--- smc/rpcclient_test.go
+++ smc/rpcclient_test.go
@@ -1,6 +1,16 @@
 package smc
 
-import "testing"
+import (
+	"context"
+	"log"
+	"testing"
+	"time"
+
+	"google.golang.org/grpc/metadata"
+
+	pbJob "github.com/grandcat/flexsmc/proto/job"
+	proto "github.com/grandcat/flexsmc/proto/smc"
+)
 
 // func Test_connect(t *testing.T) {
 // 	type args struct {
@@ -74,7 +84,79 @@ func Test_ParallelConnect(t *testing.T) {
 		tc := tc // capture range variable
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
-			connect(tc.args.socket, tc.args.id)
+			fullSMCRound(tc.args.socket, tc.args.id)
 		})
 	}
+}
+
+func fullSMCRound(socket string, id int32) error {
+	cc, err := DialSocket(socket)
+	if err != nil {
+		return err
+	}
+	defer cc.Close()
+	c := proto.NewSMCClient(cc)
+
+	ctx, _ := context.WithTimeout(context.Background(), time.Millisecond*2200)
+	// 1. Init
+	r, err := c.Init(ctx, &proto.SessionCtx{SessionID: "123456789"})
+	if err != nil {
+		log.Printf("could not SMC Init: %v", err)
+		return err
+	}
+	log.Printf("Init: %s", r)
+	// 2. Prepare (and second prepare with one missing participant)
+	md := metadata.Pairs("session-id", "123456789")
+	ctx = metadata.NewContext(ctx, md)
+
+	for i := 0; i < 2; i++ {
+		m := &pbJob.SMCCmd{
+			SmcPeerID: id,
+			Payload: &pbJob.SMCCmd_Prepare{Prepare: &pbJob.PreparePhase{
+				Participants: []*pbJob.PreparePhase_Participant{
+					&pbJob.PreparePhase_Participant{
+						SmcPeerID: 1,
+						Endpoint:  "[::1]:10001",
+					},
+					&pbJob.PreparePhase_Participant{
+						SmcPeerID: 2,
+						Endpoint:  "[::1]:10002",
+					},
+					&pbJob.PreparePhase_Participant{
+						SmcPeerID: 3,
+						Endpoint:  "[::1]:10003",
+					},
+					&pbJob.PreparePhase_Participant{
+						SmcPeerID: 4,
+						Endpoint:  "[::1]:10004",
+					},
+				},
+			}},
+		}
+		// Simulate a situation removing one node previously available
+		if i == 1 {
+			pl := m.Payload.(*pbJob.SMCCmd_Prepare).Prepare.Participants
+			pl = pl[:len(pl)-1]
+			m.Payload.(*pbJob.SMCCmd_Prepare).Prepare.Participants = pl
+		}
+
+		r, err = c.NextCmd(ctx, m)
+		if err != nil {
+			log.Fatalf("cmd for next phase failed: %v", err)
+		}
+		log.Printf("NextCmd: %s", r)
+	}
+
+	// 3. Start session
+	m := &pbJob.SMCCmd{
+		SmcPeerID: id,
+		Payload:   &pbJob.SMCCmd_Session{Session: &pbJob.SessionPhase{}},
+	}
+	r, err = c.NextCmd(ctx, m)
+	if err != nil {
+		log.Fatalf("cmd for next phase failed: %v", err)
+	}
+	log.Printf("NextCmd Session: %s", r)
+
+	return nil
 }
