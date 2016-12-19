a363608f4b0721f2683eaf624d772d015e5c234e
Stefan Smarzly <smarzlys@gmail.com>
2016-12-19 14:51:30 +0100

    chore: script to generate certificates + sample certs

--- certs/gencerts.sh
+++ certs/gencerts.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-openssl req -x509 -newkey rsa:2048 -keyout key_$1.pem -out cert_$1.pem -days 365 -nodes -extensions xauth -config /etc/ssl/openssl.cnf
+# openssl req -x509 -newkey rsa:2048 -keyout key_$1.pem -out cert_$1.pem -days 365 -nodes -extensions xauth -config /etc/ssl/openssl.cnf
+openssl req \
+    -new \
+    -newkey rsa:4096 \
+    -days 365 \
+    -nodes \
+    -x509 \
+	-extensions xauth \
+	-config /etc/ssl/openssl.cnf \
+    -subj "/C=DE/ST=BY/L=Munich/O=TUM/CN=n$1.flexsmc.local" \
+    -keyout key_$1.pem \
+    -out cert_$1.pem
