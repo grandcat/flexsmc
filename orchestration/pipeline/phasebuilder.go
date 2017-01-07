a25fa8d1c3906309105dc4ab002e85e8981f61a7
Stefan Smarzly <smarzlys@gmail.com>
2017-01-07 15:54:51 +0100

    feat(orchestration): optional Linking phase via task option

--- orchestration/pipeline/phasebuilder.go
+++ orchestration/pipeline/phasebuilder.go
@@ -18,7 +18,7 @@ func (b *PhaseBuilder) Process(task *pbJob.SMCTask, inOut *worker.JobInstruction
 	// TODO: generate unique session ID
 	sessID := "f13x0123456789"
 
-	// Prepare phase
+	// Collect configuration for Prepare phase
 	var participants []*pbJob.PreparePhase_Participant
 	for cid, p := range inOut.Participants {
 		// XXX: for testing on localhost: use continous port range
@@ -32,6 +32,7 @@ func (b *PhaseBuilder) Process(task *pbJob.SMCTask, inOut *worker.JobInstruction
 		})
 	}
 
+	// Prepare phase.
 	p1 := &pbJob.SMCCmd{
 		SessionID: sessID,
 		State:     pbJob.SMCCmd_PREPARE,
@@ -40,14 +41,31 @@ func (b *PhaseBuilder) Process(task *pbJob.SMCTask, inOut *worker.JobInstruction
 			Participants: participants,
 		}},
 	}
+	inOut.Tasks = append(inOut.Tasks, p1)
 
-	// Session phase
+	// Optional Linking phase.
+	// Improves robustness with respect to early error handling, but
+	// introduces a further round-trip to all involved peers.
+	if _, ok := task.Options["useLinkingPhase"]; ok {
+		p12 := &pbJob.SMCCmd{
+			SessionID: sessID,
+			State:     pbJob.SMCCmd_LINK,
+			Payload:   &pbJob.SMCCmd_Link{Link: &pbJob.LinkingPhase{}},
+		}
+		inOut.Tasks = append(inOut.Tasks, p12)
+		// // Not necessary to propagate to the peers as every peer will
+		// // see the phase anyways.
+		// // So, remove some bloat.
+		// delete(task.Options, "useLinkingPhase")
+	}
+
+	// Session phase.
 	p2 := &pbJob.SMCCmd{
 		SessionID: sessID,
 		State:     pbJob.SMCCmd_SESSION,
 		Payload:   &pbJob.SMCCmd_Session{Session: &pbJob.SessionPhase{}},
 	}
+	inOut.Tasks = append(inOut.Tasks, p2)
 
-	inOut.Tasks = append(inOut.Tasks, p1, p2)
 	return nil
 }
